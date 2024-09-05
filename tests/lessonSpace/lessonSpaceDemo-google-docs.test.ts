cdf2743ffe7c8b2412ad40b36cf052648a5dc3f6
Tatiana Yeremenko <tyeremenko@goguardian.com>
2024-09-04 23:49:47 -0700

    demo space is fixed

--- tests/lessonSpace/lessonSpaceDemo-google-docs.test.ts
+++ tests/lessonSpace/lessonSpaceDemo-google-docs.test.ts
@@ -23,15 +23,19 @@ describe('Visitor/Regular/Umbrella Demo Lesson Space - ', () => {
         // open Google Doc
         const [pageDoc] = await Promise.all([
             u.page.waitForEvent('popup'),
-            u.struct.demoLessonSpace.googleDocs.docs.launch.click()
-        ]);               
+            u.page.getByRole('button', { name: 'Launch Google Docs Opens an external source' }).click(),
+            u.page.waitForTimeout(1000)
+        ]); 
+                                 
         // Check url
         expect(pageDoc.url()).toContain('docs.google.com/document');
 
         // open Google Spread Sheet
         const [pageSheet] = await Promise.all([
             u.page.waitForEvent('popup'),
-            u.struct.demoLessonSpace.googleDocs.sheets.launch.click()
+            u.page.getByRole('button', { name: 'Launch Google Sheets Opens an external source' }).click(),
+            u.page.waitForTimeout(1000)
+
         ]);               
         // Check url
         expect(pageSheet.url()).toContain('docs.google.com/spreadsheets');
@@ -39,7 +43,9 @@ describe('Visitor/Regular/Umbrella Demo Lesson Space - ', () => {
         // open Google Presentation
         const [pagePres] = await Promise.all([
             u.page.waitForEvent('popup'),
-            u.struct.demoLessonSpace.googleDocs.slides.launch.click()
+            u.page.getByRole('button', { name: 'Launch Google Slides Opens an external source' }).click(),
+            u.page.waitForTimeout(1000)
+
         ]);               
         // Check url
         expect(pagePres.url()).toContain('docs.google.com/presentation');
@@ -57,3 +63,4 @@ describe('Visitor/Regular/Umbrella Demo Lesson Space - ', () => {
 
     });
 });
+
--- tests/lessonSpace/lessonSpaceDemo-header.test.ts
+++ tests/lessonSpace/lessonSpaceDemo-header.test.ts
@@ -17,22 +17,30 @@ describe('Visitor/Regular/Umbrella Demo Lesson Space - ', () => {
              await u.struct.header.userTools.username.click();
              await u.struct.userMenu.demoLessonSpace.click();
 
-            // check the logo
-            await u.struct.demoLessonSpace.header.logo.waitForVisible();
-
             // check the title
             expect(await u.struct.demoLessonSpace.header.title.text()).toBe('Demo Lesson Space: Whiteboard');
 
+            //Notes that its demo
+            await u.page.locator('//div[contains(text(),"All communication, including audio/video, is monitored. Do not share any personal information.")]').isVisible();
+            await u.page.locator('//div[contains(text(),"This is just a demo chat. No one will respond.")]').isVisible();
+
             // click on Timer
-            await u.struct.demoLessonSpace.header.timerToggle.click();
-            await u.struct.demoLessonSpace.header.timerDisplay.waitForVisible();
-      
+            await u.page.locator('//button[@aria-label="Session Timer"]').click();
+            await u.page.locator('//div[@aria-current="time"]').isVisible();
+            let start_time = await u.page.locator('//div[@aria-current="time"]').innerText();
+
             //check the timer
             function timeValidation(strTime: string) {
                 var timeFormat = /^\d?\d:\d{2}:\d{2}$/;
                 return timeFormat.test(strTime);
             }
-            timeValidation(await u.struct.demoLessonSpace.header.timerDisplay.text());
+            timeValidation( start_time);
+            await u.page.waitForTimeout(3000);
+
+            let later_time =(await u.page.locator('//div[@aria-current="time"]').innerText());
+            timeValidation( later_time);
+            expect( later_time).not.toBe(start_time);
+            console.log('start timer',start_time,' and a few secs later:',later_time);
 
             //click on Support lesson and then close it
             await u.struct.demoLessonSpace.header.support.waitForVisible();
